27956092fa4df97944f155cac4ab24c4a279fffe
fauzi ahmad <ozanfauzi39@gmail.com>
2023-06-19 19:07:58 +0700

    api buku

---
+++ app/Http/Controllers/API/BookController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        return response()->json($books);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'year' => 'required|numeric',
+        ]);
+
+        $book = Book::create($request->all());
+
+        return response()->json($book, 201);
+    }
+
+    public function show(Book $book)
+    {
+        return response()->json($book);
+    }
+
+    public function update(Request $request, Book $book)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'year' => 'required|numeric',
+        ]);
+
+        $book->update($request->all());
+
+        return response()->json($book, 200);
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +14,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::post('/login', [AuthController::class, 'index']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware('auth:sanctum')->apiResource('books', BookController::class);
